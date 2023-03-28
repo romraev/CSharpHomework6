770d7d27aa60a58f84befde2ba408706f98a739d
Romraev <romraev@yandex.ru>
2023-03-28 19:26:21 +0300

    second task

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 // Task 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int Counter(int N)
+/* int Counter(int N)
 {
     int count = 0;
     for(int i = 0; i < N; i++)
@@ -18,3 +18,33 @@ Console.Write("Сколько чисел планируете проверить
 int quan = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"Итого чисел больше 0: {Counter(quan)}");
+*/
+
+// Task 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+void Cross(double a, double b, double c, double d)
+{
+    if(a == c && b == d) Console.WriteLine("Эти одна прямая!");
+    else if(a == c) Console.WriteLine("Эти прямые параллельны"); 
+    else
+    {
+        double x = (d - b)/(a - c);
+        double y = a*x + b;
+        Console.WriteLine($"У заданных прямых точкой пересечения является x: {x}; y: {y}");
+    }
+}
+
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Cross(k1, b1, k2, b2);
