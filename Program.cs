4167cc20249841b22d03f43ee72870c8f9f6b3cc
Romraev <romraev@yandex.ru>
2023-03-28 18:31:30 +0300

    first task

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Подготовил Раевский Р.Ю.
+
+// Task 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Counter(int N)
+{
+    int count = 0;
+    for(int i = 0; i < N; i++)
+    {
+        Console.Write("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if(num > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Сколько чисел планируете проверить: ");
+int quan = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Итого чисел больше 0: {Counter(quan)}");
